d621438db72e096ded4bfcec01ed70e23ca9a844
Susan Meerdink <susan.meerdink@gmail.com>
2016-12-09 11:27:32 -0800

    Preliminary Files and code to start converting into ASTER spectral library

---
+++ Convert_to_ASTER_Spectral_Library.py
@@ -0,0 +1,30 @@
+## Convert to ASTER Spectral Library
+# Susan Meerdink
+# 12/8/2016
+# https://github.com/susanmeerdink/ASTER-Spectral-Library
+# This project contains example spectra and metadata
+# This code converts a csv of spectra and metadata into ASTER spectral library format
+# This code takes four inputs
+# 1) Metadata for spectra csv
+# 2) ASD Spectra csv - must have the same order as metadata
+# 3) Nicolet Spectra csv
+# 4) Output directory for ASTER spectral library files
+# INPUTS---------------------------------------------------------------------
+inMetaFileName = "C:\\Users\\Susan\\Documents\\GitHub\\ASTER-Spectral-Library\\Example_Metadata.csv"
+inASDFileName = "C:\\Users\\Susan\\Documents\\GitHub\\ASTER-Spectral-Library\\Example_Spectra_ASD.csv"
+inNicoletFileName = "C:\\Users\\Susan\\Documents\\GitHub\\ASTER-Spectral-Library\\Example_Spectra_Nicolet.csv"
+outDir = "F:\\Dropbox\\Analysis\\ASTER Spectral Library\\ASTER Spectral Library Files\\"
+# ---------------------------------------------------------------------
+
+import numpy as np #import numpy for array manipulation
+
+inMetaFile = open(inMetaFileName,'r') #Open metadata file
+inASDFile = open(inASDFileName,'r') #Open ASD spectra file
+inNicoletFile = open(inNicoletFileName,'r') #Open Nicolet spectra file
+
+for line in inMetaFile: #Loop through the Metadata file
+    strLine = line.split(",");
+    print (strLine)
+    
+
+
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ASTER-Spectral-Library
 Code associated with getting spectral libraries into the ASTER spectral library format
+
+## Input Spectra Format
+First row is wavelength
+First column is spectrum ID
+
+## Input Metadata Format
+First row is metadata names
